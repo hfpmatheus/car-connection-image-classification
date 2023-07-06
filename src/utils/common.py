9e79d43494652751b01df2507b47962078daff9e
hfpmatheus <matheushffp@gmail.com.br>
2023-07-06 07:23:54 -0300

    data exploration and train test split

--- src/utils/common.py
+++ src/utils/common.py
@@ -1,4 +1,14 @@
 import os
+import matplotlib.pyplot as plt
 
 def change_to_root_dir():
     os.chdir("../")
+
+def change_to_current_dir(current_dir):
+    os.chdir(current_dir)
+
+def show_image(image_array, image_title=None):
+    plt.imshow(image_array)
+    plt.title(image_title)
+    plt.axis('off')
+    plt.show()
